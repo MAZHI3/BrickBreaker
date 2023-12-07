db5b31ea9b9deae98961986f3a8b6aad07675c5f
MAZHI3 <122053601+MAZHI3@users.noreply.github.com>
2023-12-07 13:32:24 -0800

    more lives

--- scripts/level_definitions.gd
+++ scripts/level_definitions.gd
@@ -10,9 +10,10 @@ var level_1 = [
 var level_2 = [
 	[5,5,5,5,5,5,5,5],
 	[5,5,4,4,4,5,5,5],
-	[0,0,5,5,5,3,0,0],
+	[0,0,5,5,5,3,0,0]
 	
 ]
+
 
 var level_3 = [
 	[5,5,5,5,5,5,5,5],
@@ -26,3 +27,4 @@ var levels = [level_1, level_2, level_3]
 
 func get_current_level():
 	return levels[current_level - 1]
+		
